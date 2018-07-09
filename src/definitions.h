59feb42cea1f8af25736f1dee9fbb0f74f572015
Bernhard Arnold <arnobaer@users.noreply.github.com>
2018-07-09 19:39:08 +0200

    Fixed types for quantities

--- src/definitions.h
+++ src/definitions.h
@@ -1,8 +1,8 @@
 #ifndef DEFINITIONS_H_
 #define DEFINITIONS_H_
 
-const int n_algos = 16;
-const char max_obj = 12;
-const char max_requ = 4;
+const size_t n_algos = 16;
+const size_t max_obj = 12;
+const size_t max_requ = 4;
 
 #endif
