a4ee75c6d6344aa11899af8f3eea78abddffc07a
herbberg <Herbert.Bergauer@oeaw.ac.at>
2018-07-10 15:07:48 +0200

    updated conditions for 12 objects

--- src/algos.cpp
+++ src/algos.cpp
@@ -9,11 +9,11 @@ void algos (const eg_obj_t eg[max_obj], const jet_obj_t jet[max_obj], ap_uint<n_
 #pragma HLS ARRAY_PARTITION variable=eg complete dim=1
 #pragma HLS ARRAY_PARTITION variable=jet complete dim=1
 
-	const ap_uint<1> cond_double_eg_1 = comb_cond<eg_obj_requ_t,eg_obj_t,2,6>(double_eg_2_requ, eg);
-	const ap_uint<1> cond_single_jet_1 = comb_cond<jet_obj_requ_t,jet_obj_t,1,6>(single_jet_2_requ, jet);
-	const ap_uint<1> cond_triple_eg_1 = comb_cond<eg_obj_requ_t,eg_obj_t,3,6>(triple_eg_2_requ, eg);
-	const ap_uint<1> cond_triple_eg_2 = comb_cond<eg_obj_requ_t,eg_obj_t,3,6>(triple_eg_3_requ, eg);
-	const ap_uint<1> cond_quad_eg_1 = comb_cond<eg_obj_requ_t,eg_obj_t,4,6>(quad_eg_1_requ, eg);
+	const ap_uint<1> cond_double_eg_1 = comb_cond<eg_obj_requ_t,eg_obj_t,2,12>(double_eg_2_requ, eg);
+	const ap_uint<1> cond_single_jet_1 = comb_cond<jet_obj_requ_t,jet_obj_t,1,12>(single_jet_2_requ, jet);
+	const ap_uint<1> cond_triple_eg_1 = comb_cond<eg_obj_requ_t,eg_obj_t,3,12>(triple_eg_2_requ, eg);
+	const ap_uint<1> cond_triple_eg_2 = comb_cond<eg_obj_requ_t,eg_obj_t,3,12>(triple_eg_3_requ, eg);
+	const ap_uint<1> cond_quad_eg_1 = comb_cond<eg_obj_requ_t,eg_obj_t,4,12>(quad_eg_1_requ, eg);
 
 	algo = 0;
 	algo |= static_cast<ap_uint<n_algos> > (cond_double_eg_1) << 0;
--- vivado_hls.app
+++ vivado_hls.app
@@ -5,13 +5,13 @@
         <SimFlow askAgain="false" name="csim" csimMode="0" lastCsimMode="0" compiler="true"/>
     </Simulation>
     <files xmlns="">
-        <file name="../tb/algos_test_4_c_cosim.cpp" sc="0" tb="1" cflags=" "/>
-        <file name="hls4gtl/src/templates.h" sc="0" tb="false" cflags=""/>
-        <file name="hls4gtl/src/requirements.h" sc="0" tb="false" cflags=""/>
-        <file name="hls4gtl/src/obj.h" sc="0" tb="false" cflags=""/>
-        <file name="hls4gtl/src/definitions.h" sc="0" tb="false" cflags=""/>
-        <file name="hls4gtl/src/algos.h" sc="0" tb="false" cflags=""/>
+        <file name="../tb/algos_test_4_c_cosim.cpp" sc="0" tb="1" cflags="  -Wno-unknown-pragmas"/>
         <file name="hls4gtl/src/algos.cpp" sc="0" tb="false" cflags=""/>
+        <file name="hls4gtl/src/algos.h" sc="0" tb="false" cflags=""/>
+        <file name="hls4gtl/src/definitions.h" sc="0" tb="false" cflags=""/>
+        <file name="hls4gtl/src/obj.h" sc="0" tb="false" cflags=""/>
+        <file name="hls4gtl/src/requirements.h" sc="0" tb="false" cflags=""/>
+        <file name="hls4gtl/src/templates.h" sc="0" tb="false" cflags=""/>
     </files>
     <solutions xmlns="">
         <solution name="solution1" status="active"/>
