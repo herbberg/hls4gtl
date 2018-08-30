b3d8adc2ebb886aa0f29d1235ed109f81c340b23
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-08-30 17:57:24 +0200

    fixed pragma (preventing synthesis)

--- src/algos.cpp
+++ src/algos.cpp
@@ -9,7 +9,7 @@ void algos (const eg_obj_t eg[MAX_OBJ], const jet_obj_t jet[MAX_OBJ], ap_uint<N_
 {
 #pragma HLS INTERFACE ap_none port=eg
 #pragma HLS INTERFACE ap_none port=jet
-#pragma HLS INTERFACE ap_none port=algos
+#pragma HLS INTERFACE ap_none port=algo
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS ARRAY_PARTITION variable=eg complete dim=1
 #pragma HLS ARRAY_PARTITION variable=jet complete dim=1
