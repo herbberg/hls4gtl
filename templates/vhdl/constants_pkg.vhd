3c143595a88f0bb6b9130cbf43dd5485ce4a6ce1
herbberg <Herbert.Bergauer@oeaw.ac.at>
2018-09-05 16:09:28 +0200

    bug fix

--- templates/vhdl/constants_pkg.vhd
+++ templates/vhdl/constants_pkg.vhd
@@ -8,6 +8,9 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
+
+use work.lhc_data_pkg.all;
+use work.math_pkg.all;
 use work.gt_mp7_core_pkg.all;
 
 package constants_pkg is
