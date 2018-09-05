81e5163566e715bd6318b7f5cdc5a00b06de9500
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-09-05 18:36:26 +0200

    passing testvector argument to tcl script

--- run_hls.tcl
+++ run_hls.tcl
@@ -1,8 +1,12 @@
+set testvector [lindex $argv 0]
+
+delete_project hls_impl
+
 open_project hls_impl
 set_top algos
 add_files src/algos.cpp
 add_files src/algos.h
-add_files -tb tb/algos_test_4_c_cosim.cpp
+add_files -tb tb/algos_test.cpp
 add_files -tb tb/byte_vector.h
 add_files -tb tb/hex_bytes.h
 add_files -tb tb/ostream_utils.cpp
@@ -10,13 +14,15 @@ add_files -tb tb/ostream_utils.h
 add_files -tb tb/test_vector.cpp
 add_files -tb tb/test_vector.h
 add_files -tb tb/transaction_writer.h
+
 open_solution "solution1"
 set_part {xc7vx690tffg1927-2}
 create_clock -period 24 -name default
 #source "./hls4gtl/solution1/directives.tcl"
-csim_design -compiler gcc
-csynth_design
-cosim_design -trace_level port -rtl vhdl -tool xsim
-export_design -format ip_catalog
-exit
 
+csim_design -O -clean -compiler gcc -argv $testvector
+#csynth_design
+#cosim_design -trace_level port -rtl vhdl -tool xsim -argv $testvector
+#export_design -format ip_catalog
+
+exit
