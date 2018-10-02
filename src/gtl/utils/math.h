de0032fba984e13b286328e68fbed550fd870d13
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-10-02 11:15:02 +0200

    updated gtl/utils/math.h

--- src/gtl/utils/math.h
+++ src/gtl/utils/math.h
@@ -7,6 +7,11 @@ namespace gtl {
 namespace utils {
 namespace math {
 
+/* Computes the value of A raised to the power B as constant expression
+ *
+ * Example:
+ * utils::math::pow<2, 8>::value; // returns 256
+ */
 template <size_t A, size_t B>
 struct pow
 {
@@ -17,6 +22,23 @@ template <size_t A>
 struct pow<A, 0>
 {
     static const size_t value = 1;
+};
+
+/* Retruns number of bits required to store value A as constant expression
+ *
+ * Example:
+ * utils::math::bitwidth<42>::value; // returns 6
+ */
+template <size_t A>
+struct bitwidth
+{
+    static const size_t value = 1 + bitwidth<(A >> 1)>::value;
+};
+
+template <>
+struct bitwidth<0>
+{
+    static const size_t value = 0;
 };
 
 } // namespace math
