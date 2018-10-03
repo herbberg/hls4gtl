80d6fce8cde0a0fc298c27b0faba78bab17c0f4d
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-10-03 18:44:58 +0200

    fixed broken directives

--- config/directives.tcl
+++ config/directives.tcl
@@ -2,8 +2,7 @@
 
 # src/algos.cpp
 
-set_directive_interface -mode ap_none "algos" eg
-set_directive_interface -mode ap_none "algos" jet
+set_directive_interface -mode ap_none "algos" in_data
 set_directive_interface -mode ap_none "algos" algo
 
 set_directive_interface -mode ap_ctrl_none "algos"
@@ -12,6 +11,7 @@ set_directive_unroll "algos/loop_init"
 
 # src/templates.h
 
-set_directive_unroll "comb_cond_partial/loop_j"
-set_directive_unroll "comb_cond_partial/loop_k"
-set_directive_unroll "comb_cond_partial/loop_l"
+set_directive_unroll "comb/loop_i"
+set_directive_unroll "comb_partial/loop_j"
+set_directive_unroll "comb_partial/loop_k"
+set_directive_unroll "comb_partial/loop_l"
