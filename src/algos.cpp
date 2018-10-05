65715bcf577df5a722f300c0d5a835e06c37d770
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-10-05 14:57:53 +0200

    added missing pragma unroll

--- src/algos.cpp
+++ src/algos.cpp
@@ -7,6 +7,10 @@
 
 void algos(const in_data_t& in_data, ap_uint<1> algo[N_ALGORITHMS])
 {
+#pragma HLS INTERFACE ap_none port=in_data
+#pragma HLS INTERFACE ap_none port=algo
+#pragma HLS INTERFACE ap_ctrl_none port=return
+
     // create static logic
     static ::impl::conditions::logic conditions_logic = {};
     static ::impl::seeds::logic seeds_logic = {};
@@ -19,6 +23,7 @@ void algos(const in_data_t& in_data, ap_uint<1> algo[N_ALGORITHMS])
 
     loop_init: for (size_t i = 0; i <N_ALGORITHMS; i++)
     {
+#pragma HLS UNROLL
         algo[i] = 0;
     }
 
