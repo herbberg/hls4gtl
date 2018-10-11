ba3bf2d111fa2d5f43bf4a5460814d307a7f5610
herbberg <Herbert.Bergauer@oeaw.ac.at>
2018-10-11 10:23:10 +0200

    removed synthesis from IP generation in export.tcl

--- config/export.tcl
+++ config/export.tcl
@@ -3,8 +3,9 @@ source {./config/config.tcl}
 open_project $project
 open_solution $solution
 
-set flow {syn}
+#set flow {syn}
 set rtl {vhdl}
 set format {ip_catalog}
 
-export_design -flow $flow -rtl $rtl -format $format
+# export_design -flow $flow -rtl $rtl -format $format
+export_design -rtl $rtl -format $format
